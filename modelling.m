cb41078089ec8dda650fbd02bbd393758ff7d9f2
OzMashiah <ozmashiah10@gmail.com>
2023-10-08 14:30:45 +0300

    played around with the PUB and PLB and added a constraint

--- modelling.m
+++ modelling.m
@@ -23,16 +23,29 @@ for subjectNum = 1:numel(subjects)
     multiplicative = @(params, x) params(x(1)) * params(x(2));
 
     %  Define the objective function
-    objective = @(params) sum((multiplicative(params, [tParamVal, sParamVal]) - results).^2);
+    objective = @(params) sum((multiplicative(params, ...
+        [tParamVal, sParamVal]) - results).^2);
+
+    % Define the constraint function
+    % The constraint enforces the following order:
+    % params(1) > params(2) > params(3) > params(4)
+    % params(5) > params(6) > params(7) > params(8)
+    constraint = @(params) [params(1) - params(2), params(2) - params(3), ...
+        params(3) - params(4), params(5) - params(6), ...
+        params(6) - params(7), params(7) - params(8)];
+
+    % Combine the objective function and the constraint using a penalty method
+    penalized_objective = @(params) objective(params) + ...
+        sum(min(0, constraint(params)).^2);
 
     % t1, t2, t3, t4, s1, s2, s3, s4
     x0 = [0 0 0 0 0 0 0 0];                     % Starting point
     lb = [0 0 0 0 0 0 0 0];                     % Lower bounds
     ub = [1 1 1 1 1 1 1 1];                     % Upper bounds
-    plb = [0.5 0.4 0.3 0.1 0.5 0.4 0.3 0.1];    % Plausible lower bounds
-    pub = [1 0.9 0.8 0.6 1 0.9 0.8 0.6];        % Plausible upper bounds
+    plb = [0.7 0.55 0.4 0.2 0.7 0.55 0.4 0.2];    % Plausible lower bounds
+    pub = [1 0.8 0.65 0.5 1 0.8 0.65 0.5];        % Plausible upper bounds
 
-    params_fit = bads(objective, x0, lb, ub, plb, pub);
+    params_fit = bads(penalized_objective, x0, lb, ub, plb, pub);
     fittedParamsStruct.(subject) = params_fit;
 end
 
